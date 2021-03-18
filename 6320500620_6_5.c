aa87e1df16e9b79791cacf9ecb0cb3c086faf184
Litoru <puwarin2001@gmail.com>
2021-03-18 17:44:20 +0700

    6_5

--- 6320500620_6_5.c
+++ 6320500620_6_5.c
@@ -4,5 +4,24 @@
 
 void main()
 {
+    long long int w,h,j,get[2],i,alldirt=0,avg,more;
+    for(i=0 ; i<2 ; i++)
+    {
+        scanf("%lli",&get[i]);
+    }
+    w=get[0];
+    h=get[1];
+
+    for(i=0 ; i<h ; i++)
+    {
+        for(j=0 ; j<w ; j++)
+        {
+            scanf("%lli",&get[0]);
+            alldirt=alldirt+get[0];
+        }
+    }
+    avg = alldirt/(w*h);
+    more = alldirt%(w*h);
+    printf("%lli",(w*h)-more);
 
 }
