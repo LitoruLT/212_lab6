1b1f237869933ca6636be485dcb7e4ec3b45c3f5
Litoru <puwarin2001@gmail.com>
2021-03-18 18:01:51 +0700

    Update 6320500620_6_6.c

--- 6320500620_6_6.c
+++ 6320500620_6_6.c
@@ -4,5 +4,26 @@
 
 void main()
 {
+    int i,j,k,n,last,posi,posj;
+    char command[260];
+    scanf("%d",&n);
+    scanf("%s",command);
 
+
+
+    posi=0;posj=0;
+    for(i=0 ; i<n ; i++)
+    {
+        for(j=0 ; j<n ; j++)
+        {
+            for(k=0 ; command[k]!='\0' ; k++)
+            {
+                switch(command[k])
+                {
+                case 'D':
+
+                }
+            }
+        }
+    }
 }
