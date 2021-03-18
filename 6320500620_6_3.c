0994a43081c65a8e429eb460a7f0491fa34ea4d3
Litoru <puwarin2001@gmail.com>
2021-03-18 16:09:12 +0700

    update

--- 6320500620_6_3.c
+++ 6320500620_6_3.c
@@ -4,11 +4,19 @@
 
 void main()
 {
-    long long int n,q,i,j,k,get,A,B,Ptime=0,max;
-    scanf("%lli %lli",&n,&q);
-    char Command;
+    long long int n,q,i,j,k,get[2],A,B,Ptime=0,max;
+    for(i=0; i<2 ;i++)
+    {
+        scanf("%lli",&get[i]);
+    }
+    n=get[0];
+    q=get[1];
 
-    long long int Array[n];
+    printf("n=%lli q=%lli\n",Command,n,q);
+
+    char Command,getc;
+
+    long long int Array[n],ans[q];
 
     for(j=0 ; j<n ; j++)
     {
@@ -16,23 +24,46 @@ void main()
     }
 
 
+    k=0;
     for(j=0 ; j<q ; j++)
     {
         scanf("%c %lli %lli",&Command,&A,&B);
+        for(i=0; i<3 ;i++)
+        {
+            if(i==0)
+                scanf("%c",&getc);
+            else
+                scanf("%lli",&get[i-1]);
+        }
+        Command=getc;
+        A=get[0];
+        B=get[1];
+        printf("Com=%c A=%lli B=%lli\n",Command,A,B);
+
         if(Command=='U')
         {
             Array[A]=B;
         }
         else if(Command=='P')
         {
+            max=Array[A-1];
             for(i=A-1 ; i<B ; i++)
             {
-
+                if(Array[i]>max)
+                {
+                    max=Array[i];
+                }
             }
+            ans[k]=max;
+            k++;
             Ptime++;
         }
 
     }
 
+    for(i=0 ; i<Ptime ; i++)
+    {
+        printf("%lli\n",ans[i]);
+    }
 
 }
