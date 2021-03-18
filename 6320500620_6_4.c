2e06b990373d9d545b1c581acefa7f75afb6d30f
Litoru <puwarin2001@gmail.com>
2021-03-18 17:10:49 +0700

    update 6_4

--- 6320500620_6_4.c
+++ 6320500620_6_4.c
@@ -6,7 +6,7 @@ void main()
 {
     int n,i,j,k,get[2];
     scanf("%d",&n);
-    int score[n];
+    int score[n],vote[n][2];
 
     for(i=0 ; i<n ; i++)
     {
@@ -17,11 +17,45 @@ void main()
     {
         for(j=0 ; j<2 ; j++)
         {
-            scanf("%d",&get[j]);
+            scanf("%d",&vote[i][j]);
         }
-        score[get[0]]++;
-        score[get[1]]++;
     }
+    for(i=0 ; i<n ; i++)
+    {
+        for(j=0 ; j<2 ; j++)
+        {
+            score[vote[i][j]]++;
+        }
+    }
+    int max=score[0];
+    int min=score[0];
+    int whomin=0,whomax=0,rightmin=0;
+
+    while(!rightmin)
+    {
+        for(i=0 ; i<n ; i++)
+        {
+            if(score[i]>max)
+            {
+                max=score[i];
+                whomax=i;
+            }
+            else if(score[i]<min)
+            {
+                min=score[i];
+                whomin=i;
+            }
+        }
+
+        if(vote[whomax][0]==whomin || vote[whomax][1]==whomin)
+        {
+            rightmin=1;
+            break;
+        }
+
+
+    }
+    printf("%d %d",whomax,whomin);
 
 
 
