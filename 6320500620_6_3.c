f97646cf2f638b8da61018ce47397bc7485a2663
Litoru <puwarin2001@gmail.com>
2021-03-18 15:59:25 +0700

    Update 6320500620_6_3.c

--- 6320500620_6_3.c
+++ 6320500620_6_3.c
@@ -4,7 +4,35 @@
 
 void main()
 {
-    int n,i,j,k,get,posR=0,NumR=0;
-    scanf("%d",&n);
+    long long int n,q,i,j,k,get,A,B,Ptime=0,max;
+    scanf("%lli %lli",&n,&q);
+    char Command;
+
+    long long int Array[n];
+
+    for(j=0 ; j<n ; j++)
+    {
+        Array[n]=0;
+    }
+
+
+    for(j=0 ; j<q ; j++)
+    {
+        scanf("%c %lli %lli",&Command,&A,&B);
+        if(Command=='U')
+        {
+            Array[A]=B;
+        }
+        else if(Command=='P')
+        {
+            for(i=A-1 ; i<B ; i++)
+            {
+
+            }
+            Ptime++;
+        }
+
+    }
+
 
 }
