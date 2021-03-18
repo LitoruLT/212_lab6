5d53afad4caa246b54333aaa9d95671771235931
Litoru <puwarin2001@gmail.com>
2021-03-18 16:52:17 +0700

    Update 6320500620_6_4.c

--- 6320500620_6_4.c
+++ 6320500620_6_4.c
@@ -4,4 +4,26 @@
 
 void main()
 {
+    int n,i,j,k,get[2];
+    scanf("%d",&n);
+    int score[n];
+
+    for(i=0 ; i<n ; i++)
+    {
+        score[i]=0;
+    }
+
+    for(i=0 ; i<n ; i++)
+    {
+        for(j=0 ; j<2 ; j++)
+        {
+            scanf("%d",&get[j]);
+        }
+        score[get[0]]++;
+        score[get[1]]++;
+    }
+
+
+
+
 }
